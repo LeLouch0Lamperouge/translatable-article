3864bc66f149e5abff1d08782dad316437c3eb1b
Hassan El-Deghedy <106387607+LoLoch0Lamperouge@users.noreply.github.com>
2022-06-18 22:18:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ const translations = {
       en: "I enjoy creating cool websites with JavaScript.",
       ar: ". انا استمتع بانشاء مواقع رائعة باستخدام جافاسكريبت"
     },
+    fourthParagraph: {
+        en: "For me, writing codes is a hobby and a pleasure before it becomes a job.",
+        ar: ". كتابه الاكواد بالنسبه لي هوايه ومتعه قبل ان تكون عمل"
+    },
+    fifthParagraph: {
+        en: "I started writing codes in 2018.",
+        ar: ". بدئت كتابه الاكود في سنه 2018"
+    },
     readMore: {
       en: "Read More",
       ar: "اقرا المزيد"
@@ -33,6 +41,8 @@ const translations = {
   const fParagraph = document.getElementsByClassName("fParagraph")[0];
   const sParagraph = document.getElementsByClassName("sParagraph")[0];
   const tParagraph = document.getElementsByClassName("tParagraph")[0];
+  const uParagraph = document.getElementsByClassName("uParagraph")[0];
+  const iParagraph = document.getElementsByClassName("iParagraph")[0];
   const readMore = document.getElementsByClassName("readMore")[0];
 
   let languages = "en";
@@ -43,6 +53,8 @@ const translations = {
     fParagraph.innerHTML = translations.firstParagraph[languages];
     sParagraph.innerHTML = translations.secondParagraph[languages];
     tParagraph.innerHTML = translations.thirdParagraph[languages];
+    uParagraph.innerHTML = translations.fourthParagraph[languages];
+    iParagraph.innerHTML = translations.fifthParagraph[languages];
     readMore.innerHTML = translations.readMore[languages];  
   }
 
